c8b89566129949583ce0fbc452552aa8028afcb7
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2022-03-11 19:53:00 -0800

    updates to dsptar_config for more interesting sound

--- dsptar/dsptar_config.h
+++ dsptar/dsptar_config.h
@@ -7,23 +7,23 @@
 
 // Preamp gain control
 #define PREAMP_GAIN_ADC_PIN A3  // analog input pin for preamp gain control
-#define PREAMP_MIN_GAIN 50      // minimum gain value
+#define PREAMP_MIN_GAIN 5       // minimum gain value
 #define PREAMP_MAX_GAIN 500     // maximum gain value
 
 // Delay control
-#define DELAY_MAX_SECS 2                    // maximum delay time in seconds
-#define DELAY_NUM_TAPS 4                    // number of different delay taps
-#define DELAY_DELAYMS {100, 200, 300, 400}  // ms of delay on each tap (set a tap to 0 to disable)
-#define DELAY_LOG2ATTENUATION {1, 2, 3, 4}  // log 2 of attenuation of each tap (i.e. 0->gain 1, 1->gain 1/2...)
+#define DELAY_MAX_SECS 1.1                    // maximum delay time in seconds
+#define DELAY_NUM_TAPS 4                      // number of different delay taps
+#define DELAY_DELAYMS {250, 500, 750, 1000}   // ms of delay on each tap (set a tap to 0 to disable)
+#define DELAY_LOG2ATTENUATION {1, 2, 3, 4}    // log 2 of attenuation of each tap (i.e. 0->gain 1, 1->gain 1/2...)
 
 // Output noise gate control
 #define NOISE_GATE_THRESH 8192  // minimum sample absolute value to be sent to output
 
 // Output volume control
 #define VOLUME_ADC_PIN A1       // analog input pin for volume control
-#define VOLUME_GAIN 1           // constant scale factor to apply to volume
+#define VOLUME_GAIN 0.25           // constant scale factor to apply to volume
 
 // amount of audio memory, hopefully should not need to change
-#define AUDIO_MEMORY_SIZE (DELAY_QUEUE_SIZE + 10)
+#define AUDIO_MEMORY_SIZE (DELAY_QUEUE_SIZE + 20)
 
 #endif // DSPTAR_CONFIG_H
